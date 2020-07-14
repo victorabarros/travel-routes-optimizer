a48eaab70d2219cb840bb9eab94b3be4b6f97f72
victorabarros <victorabarros@github.com>
2020-07-13 23:44:20 -0300

    Finish db layer

--- internal/database/routes.go
+++ internal/database/routes.go
@@ -2,37 +2,73 @@ package database
 
 import (
 	"encoding/csv"
+	"fmt"
 	"os"
 	"strconv"
+	"time"
 )
 
-type routes map[string]map[string]int
+// Routes it's simple dictionary of travel routes
+// r[origin][destination] = price
+type Routes map[string]map[string]int
 
-func (r routes) loadCsv(csvName string) {
-	csvFile, err := os.Open(csvName)
-	defer csvFile.Close()
+// LoadCsv loads the startup csv file
+func (r Routes) LoadCsv(csvName string) {
+	fmt.Printf("Loading file %s\n", csvName)
+	csvLines, err := readCsv(csvName)
 	if err != nil {
-		panic(err)
+		panic(err) // TODO better os.fatal or logrus.fatal
 	}
 
-	csvLines, err := csv.NewReader(csvFile).ReadAll()
-	if err != nil {
-		panic(err)
-	}
+	done := make(chan error)
+	go func() {
+		done <- r.fillRoutes(csvLines)
+	}()
 
-	for _, line := range csvLines {
-		origin, destiny := line[0], line[1]
+	for {
+		for _, r := range `-\|/` {
+			select {
+			case <-done:
+				fmt.Print("\r")
+				return
+			default:
+				fmt.Printf("\r%c", r)
+				time.Sleep(100 * time.Millisecond)
+			}
+		}
+	}
+}
+
+func (r Routes) fillRoutes(lines [][]string) (err error) {
+	for idx, line := range lines {
+		orig, dest := line[0], line[1]
 		price, ok := strconv.Atoi(line[2])
 		if ok != nil {
-			panic("strcon error")
-			// TODO improve message
+			fmt.Printf("Field \"%s\", from line %d, isn't a valid value for price\n", line[2], idx+1)
+			panic("") // TODO better os.fatal or logrus.fatal
 		}
 
-		_, prs := r[origin]
+		_, prs := r[orig]
 		if !prs {
-			r[origin] = make(map[string]int)
+			r[orig] = make(map[string]int)
 		}
 
-		r[origin][destiny] = price
+		r[orig][dest] = price
 	}
+	return nil
+}
+
+// TODO: faz sentido a camada de DB abrir e um arquivo?
+func readCsv(fileName string) ([][]string, error) {
+	file, err := os.Open(fileName)
+	defer file.Close()
+	if err != nil {
+		return nil, err
+	}
+	lines, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return lines, nil
 }
