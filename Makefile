25505ef58af3bbbed62a785f0c39e595593012f8
victorabarros <victorabarros@github.com>
2020-07-14 23:56:56 -0300

    Add tests

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@
 APP_NAME?=$(shell pwd | xargs basename)
 APP_DIR = /go/src/github.com/victorabarros/${APP_NAME}
 PWD=$(shell pwd)
+DOCKER_BASE_IMAGE=golang:1.14
 
 welcome:
 	@echo "\033[33m  _______                                 _       _____                    _             " && sleep .05
@@ -25,5 +26,28 @@ clean-up:
 
 debug: welcome clean-up
 	@echo "\e[1m\033[33m\nDebug mode\e[0m"
-	docker run -it -v ${PWD}:${APP_DIR} -w ${APP_DIR} \
-		-p 8092:8092 --name ${APP_NAME} golang:1.13 bash
+	@docker run -it -v ${PWD}:${APP_DIR} -w ${APP_DIR} \
+		-p 8092:8092 --name ${APP_NAME} ${DOCKER_BASE_IMAGE} bash
+
+test:
+	@echo "\nInitalizing tests."
+	@docker run -v ${PWD}:${APP_DIR} -w ${APP_DIR} \
+		--env-file .env ${DOCKER_BASE_IMAGE} \
+		go test ./... -v -cover -race -coverprofile=c.out
+
+test-log:
+	@echo "Writing dev/tests.log"
+	@rm -rf dev/tests*.log
+	@make test > dev/tests.log
+	@echo "Writing dev/tests-summ.log"
+	@cat dev/tests.log  | grep "coverage: " > dev/tests-summ.log
+
+test-html-coverage:
+	@echo "Building c.out"
+	@rm -rf c.out
+	@make test
+	@go tool cover -html=c.out
+
+format:
+	@docker run -v ${PWD}:${APP_DIR} -w ${APP_DIR} golang gofmt -e -l -s -w .
+
---
+++ internal/database/routes_test.go
@@ -0,0 +1,79 @@
+package database
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+const (
+	testFile string = "./testSuccess.csv"
+)
+
+var (
+	testLines = []string{
+		"GRU,BRC,10",
+		"BRC,SCL,5",
+		"GRU,CDG,75",
+	}
+)
+
+func init() {
+	if err := writeCsvFile(testFile, testLines); err != nil {
+		panic(err)
+	}
+}
+
+func writeCsvFile(name string, lines []string) error {
+	f, err := os.Create(name)
+	defer f.Close()
+	if err != nil {
+		return err
+	}
+
+	for _, line := range lines {
+		f.WriteString(line + "\n")
+	}
+	return nil
+}
+
+func TestLoadCsvSuccess(t *testing.T) {
+	defer os.Remove(testFile)
+	r := Routes{}
+	r.LoadCsv(testFile)
+
+	contains := func(sl []string, elem string) bool {
+		for _, n := range sl {
+			if elem == n {
+				return true
+			}
+		}
+		return false
+	}
+
+	for orig, v := range r {
+		for dest, pric := range v {
+			join := fmt.Sprintf("%s,%s,%d", orig, dest, pric)
+			if !contains(testLines, join) {
+				t.Errorf("\"%s\" doesn't contains \"%s\"", testLines, join)
+			}
+		}
+	}
+}
+
+func TestLoadCsvInexistentFail(t *testing.T) {
+	r := Routes{}
+	if err := r.LoadCsv("./inexistent.csv"); err == nil {
+		t.Error("Must fail to read inexistent file")
+	}
+}
+
+func TestLoadCsvInvalid(t *testing.T) {
+	name := "./fail.csv"
+	writeCsvFile(name, []string{"a,b,3r"})
+	defer os.Remove(name)
+	r := Routes{}
+	if err := r.LoadCsv(name); err == nil {
+		t.Error("Must fail to load invalid price cell")
+	}
+}
